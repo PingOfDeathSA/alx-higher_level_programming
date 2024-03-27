40c5878b998d8b27d11238f38745a37e6e05ba8f
PingOfDeathSA <ronaldnt8@gmail.com>
2024-03-27 03:58:11 -0700

    lowercase

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for char_code in range(ord('a'), ord('z') + 1):
-    if chr(char_code) not in ['q', 'e']:
-        print("{:c}".format(char_code), end="")
+for hexanum in range(0, 99):
+    print("{} = {}".format(hexanum, hex(hexanum)))
