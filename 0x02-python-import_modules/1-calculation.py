c7bc5d67b95cf76f134bcd806071d474bdb17d09
PingOfDeathSA <ronaldnt8@gmail.com>
2024-03-27 05:46:06 -0700

    1-calculation.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+a = 10
+b = 5
+
+from calculator_1 import add, sub, mul, div
+
+add_result = add(a, b)
+sub_result = sub(a, b)
+mul_result = mul(a, b)
+div_result = div(a, b)
+
+print(add_result)
+print(sub_result)
+print(mul_result)
+print(div_result)
+
