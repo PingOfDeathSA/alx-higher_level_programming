1a9c8d387c909f1431a2e5ef45dfda50825e4b88
PingOfDeathSA <ronaldnt8@gmail.com>
2024-03-27 04:03:18 -0700

    hexa

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for hexanum in range(0, 99):
-    print("{} = {}".format(hexanum, hex(hexanum)))
+for lowercase in range(97, 123):
+    if chr(lowercase) is not 'q' and chr(lowercase) is not 'e':
+        print("{}".format(chr(lowercase)), end="")
---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for hexanum in range(0, 99):
+    print("{} = {}".format(hexanum, hex(hexanum)))
