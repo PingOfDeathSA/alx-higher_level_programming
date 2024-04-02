659929f531c10d2880e936d614e631d880ad7e3e
PingOfDeathSA <ronaldnt8@gmail.com>
2024-04-02 10:57:50 -0700

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+
+"""Writing class Square that defines a square"""
+
+
 class Square:
+    """Writing class Square that defines a square"""
+
     def __init__(self, size):
+        """defining fitst square.
+
+        Args:
+            size (int):
+        """
         self.__size = size
